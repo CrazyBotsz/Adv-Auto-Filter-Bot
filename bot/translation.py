78e100482b6c39a73950cc9dc916dc2f13dc76de
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-03-10 19:24:26 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,36 +5,13 @@
 class Translation(object):
     
     START_TEXT = """<b>Hai {}!!</b>
-<i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...
-
-Just Sent Any Text I Will Search In All Connected Chat And Reply You With The Message link
-
-You Can Even Connected To 3 Channels At A Time...</i>"""    
-    
-    HELP_TEXT = """<i><u>Usage Guide</u></i>
-
-<i>=> Add Bot To Any Channel As Admin With Add Members/ Invite Users Via Link
-
-=> Copy Channel ID
-
-=> Use <code>/connect {channel id}</code> In Your Group To Connect With The Group
-
-=> Use <code>/disconnect {channel id}</code> In Your Group To Disconnect From Your Group
-
-=> Use <code>/delall</code> In Your Group To Clear All Your Group  Connections (Owner Only)
-
-Now You Are All Set And Ready To Go...
-
-Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
-"""
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
-
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
-
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
-"""
+ഈ ബോട്ട് തികച്ചും <b>സിനിമ ഭ്രാന്തന്മാർ</b> ഗ്രൂപ്പിലേക്ക് ആണ്. ഞാൻ ആണ് ആ ഗ്രൂപ്പിൽ സിനിമകൾ നൽകുന്നത്😁"""
+    ABOUT_TEXT = """**➥ My Name** : ```Filter Bot```
+**➥ Creator** : <a href=https://t.me/D_ar_k_Angel>Dark Angel</a>
+**➥ Credits** : ```Everyone in this journey```
+**➥  Data base** : <a href=https://www.mongodb.com/>MongoDB</a>
+**➥ Language** : ```Python3```
+**➥ Library** : <a href=https://docs.pyrogram.org>Pyrogram asyncio 0.18.0</a>
+**➥ Source Code** : <a href=https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot>👉 Click Here</a>
+**➥ Server** : ```Heroku```
+**➥ Build Status** : ```V2.0 [BETA]```"""
